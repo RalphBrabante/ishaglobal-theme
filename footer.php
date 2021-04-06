ef24c8f0934665bf3918a0b8e81d79bc89c472a6
Ralph Brabante <ralph.b@sysgage.com>
2021-04-06 21:51:44 +0800

    footer is completed mobile resposiveness not included

--- footer.php
+++ footer.php
@@ -1,5 +1,130 @@
 
-<footer>
+<footer class="footer">
+    <div class="footer__inner">
+        <div class="footer__header">
+            <div class="footer__image-container">
+                <img class="img-responsive" src="http://localhost/ishaglobal/wp-content/uploads/2021/04/footer-logo.png" alt="White Footer Logo"/>
+            </div>
+        </div>
+
+        <div class="footer__main inline">
+            <div class="col-2">
+                <h4>Products and Solutions</h4>
+                <ul class="unordered-list">
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <h4>Pricing</h4>
+                <ul class="unordered-list">
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <h4>The Company</h4>
+                <ul class="unordered-list">
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <h4>Learn</h4>
+                <ul class="unordered-list">
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-2">
+                <h4>Performance Canvas Capabilities</h4>
+                <ul class="unordered-list">
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                    <li class="unordered-list__item">
+                        <a href="#" class="unordered-list__link">Menu Link</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="footer__copyright">
+            <p>Copyright © 2021 Isha Global Philippines | Privacy Policy |Web Design by Sysgage I.T. Solutions Co.</p>
+        </div>
+
+        <div class="footer__social">
+            <a href="#">
+                <img src="<?php echo get_template_directory_uri() . '/assets/media/SVG/facebook.svg'?>"/>
+            </a>
+            <a href="#">
+                <img src="<?php echo get_template_directory_uri() . '/assets/media/SVG/twitter.svg'?>"/>
+            </a>
+
+            <a href="#">
+                <img src="<?php echo get_template_directory_uri() . '/assets/media/SVG/youtube.svg'?>"/>
+            </a>
+
+            <a href="#">
+                <img src="<?php echo get_template_directory_uri() . '/assets/media/SVG/vimeo.svg'?>"/>
+            </a>
+
+            <a href="#">
+                <img src="<?php echo get_template_directory_uri() . '/assets/media/SVG/linkedin2.svg'?>"/>
+            </a>
+        </div>
+    </div>
+
+
+
 <?php wp_footer(); ?>
 </footer>
 
