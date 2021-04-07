8a73b038c7ff2178d583cb728be2e68341c37667
Ralph Brabante <ralph.b@sysgage.com>
2021-04-08 00:29:58 +0800

    featurette section complete

--- home-page-template.php
+++ home-page-template.php
@@ -34,10 +34,60 @@ get_header();
     <section class="get-started-section">
         <div class="get-started-section__inner">
             <div class="col col-10">
-                <h1>Performance Canvas Financials</h1>
-                <h3 class="medium-margin-top">Top-rated Financial Budgeting and Planning Software + Financial Reporting & Analysis Software</h3>
-                <p class="medium-margin-top"><strong>This Financial Budgeting and Planning software + Financial Forecasting and Reporting Software in one, unified platform improves your FP&A process by streamlining and automating financial reporting, budgeting, planning, and consolidation. When you subscribe to our solution, you subscribe to an evolving FP&A business process innovation, quality, and automation. You don´t just gain a new software; you gain new and effective team helpers.</strong></p>
-                <a href="#" class="btn-call-to-action large-margin-top">Get Started</a>
+                <div class="featurette-section__meta1">
+                    <h1>Performance Canvas Financials</h1>
+                    <h3 class="medium-margin-top">Top-rated Financial Budgeting and Planning Software + Financial
+                        Reporting
+                        & Analysis Software</h3>
+                    <p class="medium-margin-top"><strong>This Financial Budgeting and Planning software + Financial
+                            Forecasting and Reporting Software in one, unified platform improves your FP&A process by
+                            streamlining and automating financial reporting, budgeting, planning, and consolidation.
+                            When
+                            you subscribe to our solution, you subscribe to an evolving FP&A business process
+                            innovation,
+                            quality, and automation. You don´t just gain a new software; you gain new and effective team
+                            helpers.</strong></p>
+                    <a href="#" class="btn-call-to-action large-margin-top">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="featurette-section">
+        <div class="featurette-section__inner">
+            <div class="row inline">
+                <div class="col col-5 col-5-meta1">
+                    <div class="featurette-section__meta">
+                        <h3>TO THE CFO</h3>
+                        <p>We ensure you get access to reliable and real-time insights, so you can make sound and
+                            strategic
+                            business decisions.</p>
+                        <p>Subscribing to our unified financial planning software and financial reporting & analysis
+                            software will be like employing 3 extra effective & specialized consultants that will not
+                            only
+                            guide and advise you on how to best execute FP&A in your organization, it will also
+                            intelligently automate your processes elevating your FP&A process.</p>
+                    </div>
+                </div>
+                <div class="col col-5" style="background: url(<?php echo the_field('top_info_background_image'); ?>); background-position:center; background-size: cover;">
+
+                </div>
+            </div>
+            <div class="row inline">
+                <div class="col col-5" style="background: url(<?php echo the_field('top_info_background_image'); ?>); background-position:center; background-size: cover;">
+
+                </div>
+                <div class="col col-5 col-5-meta2">
+                    <div class="featurette-section__meta">
+                        <h3>TO THE FINANCE ANALYST</h3>
+                        <p>We make your FP&A process more efficient and automated, you will never dread month end close
+                            again.</p>
+                        <p>Think of our cloud financial forecasting and reporting software as a more effective yet
+                            affordable consultant who will do all the leg work with high precision, recommend to you
+                            best
+                            practices in financial planning and analysis, and who will quality check your work.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
