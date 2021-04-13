55b3d7ec8bcbd0549d01399bc1625874fcf75793
Ralph Brabante <ralph.b@sysgage.com>
2021-04-13 08:12:04 +0800

    404 page is created

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="page-wrapper">
+    <main class="page-main inline">
+        <div class="col-4 vertical-align-center horizontal-align-center large-equal-padding">
+            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/media/images/404.png">
+        </div>
+        <div class="col-6 vertical-align-center horizontal-align-center">
+            <h1><strong>404</strong> Page not found.</h1>
+        </div>
+
+    </main>
+
+</div>
+
+<?php get_footer(); ?>
